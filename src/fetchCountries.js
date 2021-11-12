56e4c789fb5aa986767f2a48f9b681109330f118
MaksStelmakh <step4ick2017@gmail.com>
2021-11-12 16:29:08 +0200

    Сделал рабочий поиск

---
+++ src/fetchCountries.js
@@ -0,0 +1,14 @@
+import Notiflix from 'notiflix';
+
+export function fetchCountries(name) {
+   return fetch(`https://restcountries.com/v3.1/name/${name}`)
+       .then(response => {
+           if (!response.ok) {
+               throw Notiflix.Notify.failure("Oops, there is no country with that name")
+           }
+           return response.json()
+       })
+}
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 import './css/styles.css';
+import { fetchCountries } from "./fetchCountries"
+import debounce from "lodash.debounce"
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const searchForm = document.querySelector(`input#search-box`)
+const countryInfo = document.querySelector(`.country-info`)
+
+
+searchForm.addEventListener(`input`, debounce(() => {
+    const name = searchForm.value.trim();
+    if (name === ``) {
+    return countryInfo.innerHTML = ``
+    }
+    fetchCountries(name).then(showCountry).catch(wrongCountry)
+    
+}, DEBOUNCE_DELAY));
+
+
+function wrongCountry(error) {
+    console.log(error)
+    return countryInfo.innerHTML = ``
+}
+
+function showCountry(arr) {
+    if (arr.length > 10) {
+    return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+    }
+    if (arr.length >= 2) {
+        let smallTemaplate = arr.map(({flags, name}) => {
+        return `<p class="name-country">
+    <img src="${flags.svg}" alt="" width="20px" height="20px">
+      ${name.official}
+      </p>`
+    }).join("")
+    return countryInfo.innerHTML = smallTemaplate
+    }
+    let template = arr.map(({flags, name, capital, population, languages}) => {
+        return `<h1 class="name-country">
+    <img src="${flags.svg}" alt="" width="20px" height="20px">
+      ${name.official}
+      </h1>
+      <ul class="list">
+      ${capital ? `<li class="country-info">
+          <span>Capital:</span> ${capital}
+        </li>` : ``}
+              ${population ? `<li class="country-info">
+          <span>Population:</span> ${population}
+        </li>` : ``}
+              ${Object.values(languages) ? `<li class="country-info">
+          <span>Languages:</span> ${Object.values(languages)}
+        </li>` : ``}
+      </ul>`
+    }).join("")
+    return countryInfo.innerHTML = template
+}
